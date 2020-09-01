2045f103ef4e31bda6d6a10fa9045e40e47e061f
sjgarza22 <sjgarza22@gmail.com>
2020-09-01 10:06:44 -0500

    Define has_many relationship, add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+    has_many :trips, through: :trip_user
 
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, length: { minimum: 2 }, uniqueness: true
+    validates :password, presence: true
 end
