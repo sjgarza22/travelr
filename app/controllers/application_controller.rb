b51f06df80ad0b2003cd092806f85a63959988f1
sjgarza22 <sjgarza22@gmail.com>
2020-09-05 19:20:04 -0500

    Add private model_params method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
     private
     
     def model_params(model_name, *args)
-        params.require(model_name).permits(*args)
+        params.require(model_name).permit(*args)
     end
+
+    
 end
