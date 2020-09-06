0ebfc3633ab819b00c692471a6167a1621eb7e57
sjgarza22 <sjgarza22@gmail.com>
2020-09-06 17:41:13 -0500

    Add trips to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+  resources :trips
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   post "/logout", to: "sessions#destroy"
