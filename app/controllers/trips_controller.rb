43e3ee18786a4e1419b26aedf4781ad354bd2e90
sjgarza22 <sjgarza22@gmail.com>
2020-09-06 18:19:49 -0500

    Build three trip_users for our new trip and set user_type to Contributer

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,9 @@
 class TripsController < ApplicationController
     def new
         @trip = Trip.new
+        @trip.trip_users.build(user_type: 'contributer')
+        @trip.trip_users.build(user_type: 'contributer')
+        @trip.trip_users.build(user_type: 'contributer')
     end
 
     def create
