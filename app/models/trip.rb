abb7c48827ade5a3c3db3622a64131d7b8abc9ef
sjgarza22 <sjgarza22@gmail.com>
2020-09-06 19:37:33 -0500

    Add has_many trip_users relationship and add user_type_selections method

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
 class Trip < ActiveRecord::Base
+    has_many :trip_users
     has_many :users, through: :trip_user
 
     # Add validations for Title:String
@@ -6,4 +7,8 @@ class Trip < ActiveRecord::Base
 
     # Add accepts_nested_attributes for :trip_user(s)
     accepts_nested_attributes_for :trip_users
+
+    def user_type_selections
+        user_types = ['Administrator', 'Contributer']
+    end
 end
