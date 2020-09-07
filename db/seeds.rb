a9eac2f3acb554b05a9c35bb49eb9feabe794728
sjgarza22 <sjgarza22@gmail.com>
2020-09-06 21:53:56 -0500

    Add three users to the Users database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'friend1@gmail.com', username: 'friend_1', password: 'password123')
+User.create(email: 'friend2@gmail.com', username: 'friend_2', password: 'password123')
+User.create(email: 'friend3@gmail.com', username: 'friend_3', password: 'password123')
