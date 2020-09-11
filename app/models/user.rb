27a37d68571720f8871c3ea4021a44795ca5f528
github username <github email address>
2020-09-10 22:02:44 -0500

    Generated Devise files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
-class User < ActiveRecord::Base
-    has_many :trip_users
-    has_many :trips, through: :trip_user
-
-    validates :email, presence: true, uniqueness: true
-    validates :username, presence: true, length: { minimum: 2 }, uniqueness: true
-    validates :password, presence: true
-
-    has_secure_password
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
