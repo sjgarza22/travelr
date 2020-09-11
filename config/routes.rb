dce9250c61c8348e52899a823c45774eabff1dde
github username <github email address>
2020-09-10 21:55:23 -0500

    Removed unnecessary routes and added route for Devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
   resources :trips
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  post "/logout", to: "sessions#destroy"
-  get "/", to: "sessions#index"
+  get "/", to: "home#index"
 end
