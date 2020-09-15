1ae60a74ae662c246a96d3ce9f44106f61f78953
sjgarza22 <sjgarza22@gmail.com>
2020-09-14 23:49:00 -0500

    Removed redirect from index and add skip_before_action so user can get to our homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
+    skip_before_action :authenticate_user!
     def index
-        if user_signed_in?
-            redirect_to trips_path
-        end
     end
 end
