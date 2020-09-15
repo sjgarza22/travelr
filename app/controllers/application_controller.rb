d26a96dba17788f67cb4baffc11225e2249f476a
sjgarza22 <sjgarza22@gmail.com>
2020-09-14 20:18:14 -0500

    Changed redirect path after Omniauth sign in to trips_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
 
     def after_sign_in_path_for(resource)
-        request.env['omniauth.origin'] || root_path
+        request.env['omniauth.origin'] || trips_path
     end
     
     private
@@ -12,5 +12,5 @@ class ApplicationController < ActionController::Base
         params.require(model_name).permit(*args)
     end
 
-    
+
 end
