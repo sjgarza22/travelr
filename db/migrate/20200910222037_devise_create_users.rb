1e7bc045ba6705e1657726f59be2edccaf4bd5f6
sjgarza22 <sjgarza22@gmail.com>
2020-09-15 00:01:20 -0500

    Removed username from Users table

--- db/migrate/20200910222037_devise_create_users.rb
+++ db/migrate/20200910222037_devise_create_users.rb
@@ -5,7 +5,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      t.string :username
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2020_09_14_114151) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "username"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
