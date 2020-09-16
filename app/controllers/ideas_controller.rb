c5d38316a7142c9045370ad88f377fcf930adc3c
sjgarza22 <sjgarza22@gmail.com>
2020-09-16 11:13:23 -0500

    Created IdeasController with new/create methods and private params method

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,3 +1,19 @@
 class IdeasController < ApplicationController
+    def new
+        @idea = Idea.new(idea_board_id: params[:idea_board_id])
+    end
 
+    def create
+        @idea = Idea.new(ideas_params)
+
+        if @idea.save
+            redirect_to idea_board_idea_path(@idea, @idea.idea_board_id)
+        end
+    end
+
+    private
+
+    def ideas_params
+        model_params(:idea, :title, :about, :price, :idea_board_id)
+    end
 end
