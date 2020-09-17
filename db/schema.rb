5ab23c8b3787fcbaf9acc0498166fce5effbeecd
sjgarza22 <sjgarza22@gmail.com>
2020-09-16 22:29:40 -0500

    Added idea_board_id to Ideas table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_114151) do
+ActiveRecord::Schema.define(version: 2020_09_15_205709) do
 
   create_table "idea_boards", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_09_14_114151) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "idea_board_id"
   end
 
   create_table "trip_users", force: :cascade do |t|
