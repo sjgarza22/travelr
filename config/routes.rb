5b5e5a0aee4b265246756dbabd66d6010024e43e
sjgarza22 <sjgarza22@gmail.com>
2020-09-16 22:28:18 -0500

    Changed nested Ideas resources to only use show and new. Create new resources for Ideas

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :trips
   
-  resources :idea_board, only: [:show] do
-    resources :ideas
+  resources :idea_boards, only: [:show] do
+    resources :ideas, only: [:show, :new]
   end
+
+  resources :ideas
 end
