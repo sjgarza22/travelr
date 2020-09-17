69805a45b5ce191974b4ad0a449f97433943a412
sjgarza22 <sjgarza22@gmail.com>
2020-09-16 22:11:52 -0500

    Defined relationships with trip_users and trips. Added Google OAuth2 to omniauth providers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  has_many :trip_users
+  has_many :trips, through: :trip_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook, :github]
+         :omniauthable, :omniauth_providers => [:facebook, :github, :google_oauth2]
 
          def self.from_omniauth(auth)
           where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
