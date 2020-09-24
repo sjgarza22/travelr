4b1a3ef6ca1107d1966295389d26b627fe0e47c6
sjgarza22 <sjgarza22@gmail.com>
2020-09-24 01:26:07 -0500

    Add methods to handle filtering for ideas by User

--- app/controllers/idea_boards_controller.rb
+++ app/controllers/idea_boards_controller.rb
@@ -1,11 +1,30 @@
 class IdeaBoardsController < ApplicationController
+    before_action :initialize_search, only: :show
+
     def show
         @idea_board = IdeaBoard.find_by(params[:id])
+        handle_filters
+    end
+
+    def clear
+        clear_session(:filter_option)
+        redirect_to idea_boards_path
     end
 
     private
 
     def idea_board_params
         model_params(:idea_board, :name, :trip_id)
+    end
+
+    def initialize_search
+        params[:filter_option] = nil if params[:filter_option].blank?
+        session[:filter_option] = params[:filter_option]
+    end
+
+    def handle_filters
+        if session[:filter_option]
+            @user = Idea.filter_by_user(session[:filter_option])
+        end
     end
 end
