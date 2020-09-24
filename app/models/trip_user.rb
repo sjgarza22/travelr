d8a35e3f92b9a4643f8ef614992cfb36f50b01dc
sjgarza22 <sjgarza22@gmail.com>
2020-09-24 01:30:33 -0500

    Define has_many relationship with Ideas

--- app/models/trip_user.rb
+++ app/models/trip_user.rb
@@ -1,6 +1,7 @@
 class TripUser < ActiveRecord::Base
     belongs_to :trip
     belongs_to :user
+    has_many :ideas
 
     def user_type_selections
         user_types = ['Administrator', 'Contributer']
